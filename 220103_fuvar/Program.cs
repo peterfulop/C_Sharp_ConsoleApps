b3ff721133d59971f32a883cecb514c1783c7d83
mrpeterfulop@gmail.com <mrpeterfulop@gmail.com>
2022-01-10 19:40:26 +0100

    Update

---
+++ 220103_fuvar/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _220103_fuvar
+{
+    class Fuvar
+    {
+        public int taxi_id { get; set; }
+        public string indulas { get; set; }
+        public double idotartam { get; set; }
+        public double tavolsag { get; set; }
+        public double viteldij { get; set; }
+        public double borravalo { get; set; }
+        public string fizetes_modja { get; set; }
+
+
+    }
+    internal class Program
+    {
+        public static List<Fuvar> Fuvarok = new List<Fuvar>();
+        public static string Header { get; set; }
+        static void Main(string[] args)
+        {
+
+            Feladat_02();
+            Feladat_03();
+            Feladat_04();
+            Feladat_05();
+            Feladat_06();
+            Feladat_07();
+            Feladat_08();
+
+            Console.ReadLine();
+        }
+
+        private static void Feladat_08()
+        {
+            var hibas = Fuvarok.Where(x => x.idotartam > 0 && x.viteldij > 0 && x.tavolsag == 0).ToList().OrderBy(x => x.indulas);
+
+            using (var fs = new FileStream("Hibak.txt", FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    sw.WriteLine(Header);
+
+                    foreach (var h in hibas)
+                    {
+                        var line = $"{h.taxi_id};{h.indulas};{h.idotartam};{h.tavolsag};{h.viteldij};{h.borravalo};{h.fizetes_modja}";
+                        sw.WriteLine(line);
+                    }
+
+                }
+            }
+
+            Console.WriteLine($"8. feladat: hibak.txt");
+        }
+
+        private static void Feladat_07()
+        {
+            var leghosszabb = Fuvarok.OrderByDescending(x => x.idotartam).First();
+
+            Console.WriteLine($"6. feladat: Leghosszabb fuvar:");
+            Console.WriteLine($"\tFuvar hossza: {leghosszabb.idotartam} másodperc");
+            Console.WriteLine($"\tTaxi azonosító: {leghosszabb.taxi_id}");
+            Console.WriteLine($"\tMegtett távolság:{leghosszabb.tavolsag * 1,6:#.##} km");
+            Console.WriteLine($"\tViteldíj: {leghosszabb.viteldij}$");
+
+        }
+
+        private static void Feladat_06()
+        {
+            var osszesUt = Fuvarok.Sum(x => x.tavolsag) * 1.6;
+            Console.WriteLine($"6. feladat: {osszesUt:#.##} km");
+        }
+
+        private static void Feladat_05()
+        {
+            var fizetesek = Fuvarok.GroupBy(x => x.fizetes_modja)
+                .Select(x => new { fizetesiMod = x.Key, count = x.Count() });
+            Console.WriteLine("5. feladat:");
+            foreach (var m in fizetesek)
+            {
+                Console.WriteLine($"\t{m.fizetesiMod}: {m.count} fuvar");
+            }
+        }
+
+        private static void Feladat_04()
+        {
+            var taxis = Fuvarok.Where(x => x.taxi_id == 6185);
+            Console.WriteLine($"4. feladat: {taxis.Count()} fuvar alatt: {taxis.Sum(x => x.borravalo) + taxis.Sum(x => x.viteldij)}$");
+        }
+
+        private static void Feladat_03()
+        {
+            Console.WriteLine($"3. feladat: {Fuvarok.Count} fuvar");
+        }
+
+        private static void Feladat_02()
+        {
+            using (var fs = new FileStream("fuvar.csv", FileMode.Open))
+            {
+                using (var sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    Header = sr.ReadLine();
+
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine().Split(';');
+
+                        var newFuvar = new Fuvar()
+                        {
+                            taxi_id = int.Parse(line[0]),
+                            indulas = line[1],
+                            idotartam = Convert.ToDouble(line[2]),
+                            tavolsag = Convert.ToDouble(line[3]),
+                            viteldij = Convert.ToDouble(line[4]),
+                            borravalo = Convert.ToDouble(line[5]),
+                            fizetes_modja = line[6]
+                        };
+
+                        Fuvarok.Add(newFuvar);
+
+                    }
+                }
+            }
+
+        }
+    }
+
+}
---
+++ 220110_nyelvvizsga/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _220110_nyelvvizsga
+{
+    class Vizsga
+    {
+        public string Nyelv { get; set; }
+        public int Ev { get; set; }
+        public int Osszeg { get; set; }
+        public bool Sikeres { get; set; }
+    }
+
+    internal class Program
+    {
+        public static List<int> Evek = new List<int>();
+        public static List<Vizsga> Vizsgak = new List<Vizsga>();
+        static void Main(string[] args)
+        {
+            Feladat_01();
+            Feladat_02();
+            var ev = Feladat_03();
+            Feladat_04(ev);
+            Feladat_05(ev);
+            Feladat_06();
+            Console.ReadLine();
+        }
+        private static void Feladat_06()
+        {
+            var atlag = Vizsgak.GroupBy(x => x.Nyelv)
+                .Select(x => new {
+                    Key = x.Key,
+                    sum = x.Sum(y => y.Osszeg),
+                    sikeres = x.Where(y => y.Sikeres).Sum(y => y.Osszeg),
+                    sikertelen = x.Where(z => z.Sikeres == false).Sum(z => z.Osszeg)
+                });
+
+            using (var fs = new FileStream("osszesites.csv", FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    foreach (var v in atlag)
+                    {
+                        float arany = (float)v.sikertelen / v.sum * 100;
+                        var line = $"{v.Key};{v.sum};{arany:0.00}%";
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+        }
+        private static void Feladat_05(int ev)
+        {
+            Console.WriteLine("5. feladat:");
+            var nemvolt = Vizsgak.Where(x => x.Ev == ev && x.Osszeg == 0).GroupBy(x => x.Nyelv).ToList();
+
+            if (nemvolt.Count() == 0)
+            {
+                Console.WriteLine("Minden nyelvből volt vizsgázó!");
+            }
+            else
+            {
+                nemvolt.ForEach(x => Console.WriteLine($"\t{x.Key}"));
+            }
+        }
+        private static void Feladat_04(int ev)
+        {
+            Console.WriteLine("4. feladat:");
+
+            var sikertelen = Vizsgak.Where(x => x.Sikeres == false && x.Ev == ev)
+                .OrderByDescending(x => x.Osszeg).First();
+
+            var osszesen = Vizsgak.FirstOrDefault(x => x.Nyelv == sikertelen.Nyelv && x.Ev == ev).Osszeg;
+
+            double arany = (double)sikertelen.Osszeg / (sikertelen.Osszeg + osszesen) * 100;
+
+            Console.WriteLine($"{ev}-ben {sikertelen.Nyelv} nyelvből a sikertelen vizsgák aránya {arany:0.00} %");
+        }
+        private static int Feladat_03()
+        {
+            Console.WriteLine("3. feladat:");
+            Console.Write("\tVizsgálandó év:");
+            var ev = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(ev) || int.Parse(ev) < 2009 || int.Parse(ev) > 2017)
+            {
+                Console.WriteLine("2009 és 2017 közötti értéket adj meg!");
+                Console.Write("Vizsgálandó év:");
+                ev = Console.ReadLine();
+            }
+            return int.Parse(ev);
+        }
+        private static void Feladat_02()
+        {
+            var leg = Vizsgak
+                .GroupBy(x => x.Nyelv)
+                .Select(x => new { Key = x.Key, sum = x.Sum(y => y.Osszeg) })
+                .OrderByDescending(x => x.sum)
+                .ToList();
+
+            Console.WriteLine("2. feladat: A legnépszerűbb nyelvek:");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"\t{leg[i].Key}");
+            }
+        }
+        private static void Feladat_01()
+        {
+            fileReading("sikeres.csv", true);
+            fileReading("sikertelen.csv", false);
+        }
+        private static void fileReading(string fileName, bool sikeres)
+        {
+            using (var fs = new FileStream(fileName, FileMode.Open))
+            {
+                using (var sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    var header = sr.ReadLine().Split(';');
+
+                    foreach (var item in header)
+                    {
+                        if (item.StartsWith("2"))
+                        {
+                            Evek.Add(int.Parse(item));
+                        }
+                    }
+
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine().Split(';');
+
+                        for (int i = 1; i < line.Length; i++)
+                        {
+                            var newVizsga = new Vizsga();
+                            newVizsga.Nyelv = line[0];
+                            newVizsga.Sikeres = sikeres;
+                            newVizsga.Ev = Evek[i - 1];
+                            newVizsga.Osszeg = int.Parse(line[i]);
+                            Vizsgak.Add(newVizsga);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
