6fed564766a13b98f3c022c6b7b5297a9af219c5
mrpeterfulop@gmail.com <mrpeterfulop@gmail.com>
2022-01-26 20:23:21 +0100

    01.26. EUtagok, koridok

---
+++ 220126_EU_tagallamok/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _220126_EU_tagallamok
+{
+    class EU
+    {
+        public string Orszag { get; set; }
+        public DateTime Csatlakozas { get; set; }
+    }
+    internal class Program
+    {
+        public static List<EU> EUtagok = new List<EU>();
+
+        static void Main(string[] args)
+        {
+
+            Feladat_02();
+
+            Feladat_03();
+
+            Feladat_04();
+
+            Feladat_05();
+
+            Feladat_06();
+            
+            Feladat_07();
+
+            Feladat_08();
+
+            Console.ReadLine();
+        }
+
+        private static void Feladat_08()
+        {
+            Console.WriteLine($"8. feladat: Statisztika");
+            EUtagok.GroupBy(x=>x.Csatlakozas)
+                .Select(x => new {Ev = x.Key.Year, Count = x.Count()})
+                .Where(x => x.Count > 0)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"\t{x.Ev} - {x.Count} ország"));
+        }
+
+        private static void Feladat_07()
+        {
+            Console.WriteLine($"7. feladat: Legutoljára csatlakozott ország: {EUtagok.OrderBy(x=>x.Csatlakozas).Last().Orszag}");
+        }
+
+        private static void Feladat_06()
+        {
+            var majus = EUtagok.Exists(x => x.Csatlakozas.Month == 5) ? "volt" : "nem volt";
+            Console.WriteLine($"6. feladat: Májusban {majus} csatlakozás!");
+        }
+
+        private static void Feladat_05()
+        {
+            Console.WriteLine($"5. feladat: Magyarország csatlakozásának dátuma: {EUtagok.First(x => x.Orszag == "Magyarország").Csatlakozas.ToShortDateString()}");
+        }
+
+        private static void Feladat_04()
+        {
+            Console.WriteLine($"4. feladat: 2007-ben {EUtagok.Count(x=>x.Csatlakozas.Year ==2007)} ország csatlakozott.");
+        }
+
+        private static void Feladat_03()
+        {
+            Console.WriteLine($"3. feladat: EU tagállamok száma: {EUtagok.Count} db");
+        }
+
+        private static void Feladat_02()
+        {
+            using (var fs = new FileStream("EUcsatlakozas.txt",FileMode.Open))
+            {
+                using (var sr = new StreamReader(fs,Encoding.UTF8))
+                {
+
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine().Split(';');
+                        var ujTag = new EU()
+                        {
+                            Orszag = line[0],
+                            Csatlakozas = Convert.ToDateTime(line[1])
+                        };
+                        EUtagok.Add(ujTag);
+                    }                   
+                }
+            }
+        }
+    }
+}
---
+++ 220126_koridok/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _220126_koridok
+{
+
+    class Verseny
+    {
+        public string Csapat { get; set; }
+        public string Versenyzo { get; set; }
+        public int Eletkor { get; set; }
+        public string Palya { get; set; }
+        public DateTime Korido { get; set; }
+        public int Kor { get; set; }
+
+    }
+
+    internal class Program
+    {
+        public static List<Verseny> VersenyLista = new List<Verseny>();
+        static void Main(string[] args)
+        {
+            Feladat_02();
+
+            Feladat_03();
+
+            Feladat_04();
+
+            var versenyzo = Feladat_05();
+
+            Feladat_06(versenyzo);
+
+            Console.ReadLine();
+        }
+
+        private static void Feladat_06(string versenyzo)
+        {
+            if (!VersenyLista.Exists(x => x.Versenyzo == versenyzo))
+            {
+                Console.WriteLine("6. feladat: Nincs ilyen versenyző az állományban!");
+                return;
+            }
+            var best = VersenyLista.Where(x=>x.Versenyzo == versenyzo)
+                .OrderBy(x=>x.Korido)
+                .First();
+
+            Console.WriteLine($"6. feladat: {best.Versenyzo}, {best.Korido.ToLongTimeString()}");
+        }
+
+        private static string Feladat_05()
+        {
+            Console.WriteLine("Kérem egy versenyző nevét:");
+            return Console.ReadLine();
+        }
+
+        private static void Feladat_04()
+        {
+            var korido = VersenyLista.First(x => x.Versenyzo == "Fürge Ferenc" &&
+            x.Palya == "Gran Prix Circuit" &&
+            x.Kor == 3)
+                .Korido;
+
+            Console.WriteLine($"4. feladat: {korido.Hour*60*60+ korido.Minute*60 + korido.Second} másodperc");
+        }
+
+        private static void Feladat_03()
+        {
+            Console.WriteLine($"3. feladat: {VersenyLista.Count}");
+        }
+
+        private static void Feladat_02()
+        {
+            using (var fs = new FileStream("autoverseny.csv",FileMode.Open))
+            {
+                using (var sr = new StreamReader(fs,Encoding.UTF8))
+                {
+                    sr.ReadLine();
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine().Split(';');
+                        var verseny = new Verseny()
+                        {
+                            Csapat = line[0],
+                            Versenyzo = line[1],
+                            Eletkor = int.Parse(line[2]),
+                            Palya = line[3],
+                            Korido = Convert.ToDateTime(line[4]),
+                            Kor = int.Parse(line[5]),
+                        };
+
+                        VersenyLista.Add(verseny);
+                    }
+                }
+            }
+        }
+    }
+}
