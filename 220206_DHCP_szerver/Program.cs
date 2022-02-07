c1a1ba38344688659be9e1e14f746538fd3b0fe8
mrpeterfulop@gmail.com <mrpeterfulop@gmail.com>
2022-02-07 15:15:52 +0100

    02.07. DHCP szerver

--- 220206_DHCP_szerver/Program.cs
+++ 220206_DHCP_szerver/Program.cs
@@ -7,46 +7,120 @@ using System.Threading.Tasks;
 
 namespace _220206_DHCP_szerver
 {
-
-
+    class Test
+    {
+        public string Parameter { get; set; }
+        public string Value { get; set; }
+    }
     class DHCP
     {
         public string Mac { get; set; }
         public string IP { get; set; }
-        public bool Excluded { get; set; }
-        public bool Reserved { get; set; }
     }
     internal class Program
     {
-        public static List<DHCP> DHCPs = new List<DHCP>();
 
         public static List<string> Excluded = new List<string>();
         public static List<string> EnabledIps = new List<string>();
 
+        public static List<DHCP> Reserved = new List<DHCP>();
+        public static List<DHCP> Actual = new List<DHCP>();
+
+        public static List<Test> TestList = new List<Test>();
+
 
         static void Main(string[] args)
         {
-            // 192.168.10.100 - 192.168.10.199
-
-            ReadExcluded();
-            ReadReserved();
-            ReadActual();
-
-            EnabledIps = CreateIpList();
-
-            DHCPs.ForEach(dhcp => Console.WriteLine($"{dhcp.Mac}, {dhcp.IP}, {dhcp.Excluded}, {dhcp.Reserved}"));
+            ReadSourceFiles();
+            RunDHCP();
+            WriteActualsToFile();
 
             Console.ReadLine();
         }
 
-        private static List<string> CreateIpList()
+
+        private static void WriteActualsToFile()
+        {
+            using (var fs = new FileStream("dhcp_kesz.csv", FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    foreach (var l in Actual.OrderBy(x=>x.IP))
+                    {
+                        sw.WriteLine($"{l.Mac};{l.IP}");
+                    }
+                }
+            }
+
+            Console.WriteLine("3. feladat: Adatok kiírva!");
+        }
+
+        private static void RunDHCP()
+        {
+            Console.WriteLine("2. feladat: testlista feldolgozása...");
+
+            foreach (var i in TestList)
+            {
+                if (i.Parameter == "request")
+                {
+                    if (!IsExcluded(i.Value) && IsReserved(i.Value))
+                    {
+                        var getReservedIP = Reserved.FirstOrDefault(x => x.Mac == i.Value).IP;
+                        Actual.Add(new DHCP() { IP = getReservedIP, Mac = i.Value });
+                    }
+                    else
+                    {
+                        if (EnabledIps.Count != 0)
+                        {
+                            var firstEnabledIP = EnabledIps.OrderBy(x => x).First();
+                            Actual.Add(new DHCP() { IP = firstEnabledIP, Mac = i.Value });
+                            var remove = EnabledIps.FirstOrDefault(x => x == firstEnabledIP);
+                            EnabledIps.Remove(remove);
+                        }
+                    }
+                }
+                else
+                {
+                    if (!IsExcluded(i.Value))
+                    {
+                        var remove = Actual.FirstOrDefault(x => x.IP == i.Value);
+                        Actual.Remove(remove);
+
+                        if (!IsReserved(i.Value, false))
+                        {
+                            EnabledIps.Add(i.Value);
+                            EnabledIps.OrderBy(x => x);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool IsReserved(string parameter, bool mac=true)
+        {
+            if (mac)
+            {
+                return Reserved.Select(x=>x.Mac).Contains(parameter);
+            }
+            else
+            {
+                return Reserved.Select(x => x.IP).Contains(parameter);
+            }
+        }
+
+        private static bool IsExcluded(string parameter)
+        {
+            return Excluded.Contains(parameter);
+        }
+
+        private static List<string> GetEnabledIPs()
         {
             var list = new List<string>();
             for (int i = 100; i < 200; i++)
             {
                 var newIp = $"192.168.10.{i}";
 
-                if (!Excluded.Contains(newIp))
+                if (!Excluded.Contains(newIp) && !Reserved.Select(x=>x.IP).Contains(newIp) && !Actual.Select(x => x.IP).Contains(newIp))
                 {
                     list.Add(newIp);
                 }
@@ -54,6 +128,37 @@ namespace _220206_DHCP_szerver
             return list;
         }
 
+        private static void ReadSourceFiles()
+        {
+            Console.WriteLine("1. feladat: forrásfileok beolvasása...");
+            ReadExcluded();
+            ReadReserved();
+            ReadActual();
+            ReadTest();
+            EnabledIps = GetEnabledIPs();
+
+        }
+
+        private static void ReadTest()
+        {
+            using (var fs = new FileStream("test.csv", FileMode.Open))
+            {
+                using (var sr = new StreamReader(fs, Encoding.UTF8))
+                {
+
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine().Split(';');
+                        var test = new Test()
+                        {
+                            Parameter = line[0],
+                            Value = line[1],
+                        };
+                        TestList.Add(test);
+                    }
+                }
+            }
+        }
         private static void ReadActual()
         {
             using (var fs = new FileStream("dhcp.csv", FileMode.Open))
@@ -64,14 +169,12 @@ namespace _220206_DHCP_szerver
                     while (!sr.EndOfStream)
                     {
                         var line = sr.ReadLine().Split(';');
-                        var ujDHCP = new DHCP()
+                        var ujActual = new DHCP()
                         {
                             Mac = line[0],
                             IP = line[1],
-                            Excluded = false,
-                            Reserved = false
                         };
-                        DHCPs.Add(ujDHCP);
+                        Actual.Add(ujActual);
                     }
 
                 }
@@ -87,14 +190,12 @@ namespace _220206_DHCP_szerver
                     while (!sr.EndOfStream)
                     {
                         var line = sr.ReadLine().Split(';');
-                        var ujDHCP = new DHCP()
+                        var ujReserved = new DHCP()
                         {
                             Mac = line[0],
                             IP = line[1],
-                            Excluded = false,
-                            Reserved = true
                         };
-                        DHCPs.Add(ujDHCP);
+                        Reserved.Add(ujReserved);
                     }
 
                 }
